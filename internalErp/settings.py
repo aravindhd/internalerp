e7597319ff5ce125893fd4cc4325e1b8b8e8f84d
aravindhd <aravindhd@embedur.com>
2016-04-11 14:35:52 +0530

    Commiting the PRODUCTION SITE Setup related critical Changes

--- internalErp/settings.py
+++ internalErp/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'internalErp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'internalerpdb',
+        'USER': 'embeduradmin',
+        'PASSWORD': 'Christy94538',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
--- internalErp/wsgi.py
+++ internalErp/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/embeduradmin/eur-internal/internalerp')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/embeduradmin/eur-internal/lib/site-packages')
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "internalErp.settings")
 
